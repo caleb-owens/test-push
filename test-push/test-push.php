65299c220575bf72dcc1e4ed1d5ff7501b2fbdce
Caleb Owens <caleb.q.owens@gmail.com>
2021-05-19 14:20:44 -0700

    5th commit

--- test-push/test-push.php
+++ test-push/test-push.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Test Push
  * Plugin URI:        https://andersonadvisors.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.2.0
+ * Version:           1.3.0
  * Author:            Caleb Owens
  * Author URI:        https://github.com/caleb-owens
  * License:           GPL-2.0+
